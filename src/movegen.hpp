2d560ae76eb4f6d74cc9b6fa7b35f490451e415b
Olivia <95939886+omarkham@users.noreply.github.com>
2023-07-26 23:49:51 -0400

    Create movegen.hpp

---
+++ src/movegen.hpp
@@ -0,0 +1,22 @@
+#ifndef MOVEGEN_HPP
+#define MOVEGEN_HPP
+
+#include "board.hpp"
+
+
+// Forward declarations for the piece-specific move generation functions
+std::vector<Move> generatePawnMoves(const Board& board, int srcRow, int srcCol);
+std::vector<Move> generateRookMoves(const Board& board, int srcRow, int srcCol);
+std::vector<Move> generateKnightMoves(const Board& board, int srcRow, int srcCol);
+std::vector<Move> generateBishopMoves(const Board& board, int srcRow, int srcCol);
+std::vector<Move> generateQueenMoves(const Board& board, int srcRow, int srcCol);
+std::vector<Move> generateKingMoves(const Board& board, int srcRow, int srcCol);
+
+std::vector<Move> generateMovesForPiece(const Board& board, int row, int col);
+
+bool isSquareAttacked(const Board& board, int row, int col, PieceColor attackingColor);
+
+
+PieceColor getOppositeColor(PieceColor color);
+
+#endif // MOVEGEN_HPP
