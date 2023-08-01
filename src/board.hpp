3e75919249e45b2578804e9a3e2838d454b59361
Olivia <95939886+omarkham@users.noreply.github.com>
2023-08-01 10:48:49 -0400

    Update board.hpp

--- src/board.hpp
+++ src/board.hpp
@@ -18,9 +18,23 @@ public:
     bool isValidPosition(int row, int col) const;
     bool isEmpty(int row, int col) const;
     void removePiece(int row, int col, PieceColor color);
+    bool isCheckmate(PieceColor color) const;
+    bool isCheck(PieceColor color) const;
+    bool isSquareAttacked(int row, int col, PieceColor attackingColor) const;
+    bool isStalemate() const;
+    bool isGameOver() const;
+    bool isInsufficientMaterials() const;
+    bool isThreefoldRepetition() const;
+    void makeAIMove();
 
+    PieceColor getCurrentPlayer() const;
 	PieceType getPieceType(int row, int col) const;
 	PieceColor getPieceColor(int row, int col) const;
+
+    //Add a copy constructor and assignment operator
+    Board(const Board& board) = default;
+    Board& operator=(const Board& other) = delete;
+
 private:
     uint64_t whitePawns;
     uint64_t whiteKnights;
@@ -35,6 +49,11 @@ private:
     uint64_t blackQueens;
     uint64_t blackKing;
 
+    mutable std::vector<uint64_t> previousBoardStates;
+
+    static int popcount64(uint64_t n);
+
+    PieceColor currentPlayer;
 };
 
 #endif //BOARD_HPP
