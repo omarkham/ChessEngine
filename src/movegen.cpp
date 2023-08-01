b38cc8fd810dc95d2fee6fe6f74030803fd83817
Olivia <95939886+omarkham@users.noreply.github.com>
2023-08-01 10:50:05 -0400

    Update movegen.cpp

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -196,7 +196,7 @@ std::vector<Move> generateKingMoves(const Board& board, int srcRow, int srcCol) 
             if (targetPieceColor != pieceColor) {
                 //Check if the destination square is under attack by the opponent
                 //If it's not under attack, it's a valid move for the king
-                if (!isSquareAttacked(board, destRow, destCol, getOppositeColor(pieceColor))) {
+                if (board.isSquareAttacked(destRow, destCol, getOppositeColor(pieceColor)) == false) {
                     moves.push_back({ srcRow, srcCol, destRow, destCol,
                         (targetPieceColor == PieceColor::EMPTY) ? MoveType::QUIET : MoveType::CAPTURE });
                 }
@@ -211,139 +211,11 @@ PieceColor getOppositeColor(PieceColor color) {
     return (color == PieceColor::WHITE) ? PieceColor::BLACK : PieceColor::WHITE;
 }
 
-// Function to check if a square is under attack by an opponent's piece
-bool isSquareAttacked(const Board& board, int row, int col, PieceColor attackingColor) {
-    int dr[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
-    int dc[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-    // Check for non-sliding attacks (knights, kings, and pawns)
-    for (int i = 0; i < 8; ++i) {
-int targetRow = row + dr[i];
-int targetCol = col + dc[i];
-if (board.isValidPosition(targetRow, targetCol)) {
-    PieceColor targetPieceColor = board.getPieceColor(targetRow, targetCol);
-    PieceType targetPieceType = board.getPieceType(targetRow, targetCol);
-    if (targetPieceColor == attackingColor) {
-        if (targetPieceType == PieceType::KNIGHT) {
-            if ((std::abs(dr[i]) == 2 && std::abs(dc[i]) == 1) || (std::abs(dr[i]) == 1 && std::abs(dc[i]) == 2)) {
-                return true;
-            }
-        }
-        else if (targetPieceType == PieceType::KING) {
-            if (std::abs(dr[i]) <= 1 && std::abs(dc[i]) <= 1) {
-                return true;
-            }
-        }
-        else if (targetPieceType == PieceType::PAWN) {
-            if (attackingColor == PieceColor::WHITE) {
-                if (i == 4 && dr[i] == -1 && dc[i] == -1) { // White pawn capturing to the top-left
-                    return true;
-                }
-                else if (i == 5 && dr[i] == -1 && dc[i] == 1) { // White pawn capturing to the top-right
-                    return true;
-                }
-            }
-            else {
-                if (i == 6 && dr[i] == 1 && dc[i] == -1) { // Black pawn capturing to the bottom-left
-                    return true;
-                }
-                else if (i == 7 && dr[i] == 1 && dc[i] == 1) { // Black pawn capturing to the bottom-right
-                    return true;
-                }
-            }
-        }
-    }
-}
-    }
-
-    // Check for sliding attacks (rooks, bishops, and queens)
-    int slidingDirections[] = { -1, 1, 0 }; // -1 represents negative direction, 1 represents positive direction, 0 represents no movement
-    for (int drDir : slidingDirections) {
-        for (int dcDir : slidingDirections) {
-            if (drDir == 0 && dcDir == 0) continue; // Skip the case where both directions are 0 (no movement)
-            for (int step = 1; step < 8; ++step) {
-                int targetRow = row + drDir * step;
-                int targetCol = col + dcDir * step;
-                if (!board.isValidPosition(targetRow, targetCol)) break; // Stop if the target square is not valid
-                PieceColor targetPieceColor = board.getPieceColor(targetRow, targetCol);
-                if (targetPieceColor == attackingColor) {
-                    PieceType targetPieceType = board.getPieceType(targetRow, targetCol);
-                    if (targetPieceType == PieceType::ROOK || targetPieceType == PieceType::QUEEN) {
-                        return true;
-                    }
-                    else {
-                        // We reached a piece that is of the same color, no need to check further in this direction
-                        break;
-                    }
-                }
-                else if (targetPieceColor != PieceColor::EMPTY) {
-                    // We reached a piece of the opponent's color that is not a rook or queen, no need to check further in this direction
-                    break;
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
-//Check if a player is in check
-bool isCheck(const Board& board, PieceColor color) {
-    int kingRow = -1;
-    int kingCol = -1;
-    for (int row = 0; row < 8; ++row) {
-        for (int col = 0; col < 8; ++col) {
-            if (board.getPieceType(row, col) == PieceType::KING && board.getPieceColor(row, col) == color) {
-                kingRow = row;
-                kingCol = col;
-                break;
-            }
-        }
-    }
-
-    if (kingRow == -1 || kingCol == -1) {
-        //King not found, something's wrong with the board
-        return false;
-    }
-    //Check if the square where the king is located is under attack by the opponent
-    return isSquareAttacked(board, kingRow, kingCol, getOppositeColor(color));
-}
-
-//Check if a player is in checkmate
-bool isCheckmate(const Board& board, PieceColor color) {
-    if (!isCheck(board, color)) {
-        return false;
-    }
-
-    //Check if the player has any legal moves to get out of check
-    for (int row = 0; row < 8; ++row) {
-        for (int col = 0; col < 8; ++col) {
-            if (board.getPieceColor(row, col) == color) {
-                std::vector<Move> moves = generateMovesForPiece(board, row, col);
-                for (const Move& move : moves) {
-                    int srcRow = move.srcRow;
-                    int srcCol = move.srcCol;
-                    int destRow = move.destRow;
-                    int destCol = move.destCol;
-
-                    Board newBoard = board;
-                    newBoard.makeMove(srcRow, srcCol, destRow, destCol);
-                    if (!isCheck(newBoard, color)) {
-                        //Found a legal move to get out of check
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    return true;
-}
 
 // Function to generate moves for a piece and apply them to a copy of the board
 std::vector<Move> generateMovesForPiece(const Board& board, int row, int col)
 {
     PieceType pieceType = board.getPieceType(row, col);
-    PieceColor pieceColor = board.getPieceColor(row, col);
 
     if (pieceType == PieceType::PAWN) {
         return generatePawnMoves(board, row, col);
@@ -368,3 +240,16 @@ std::vector<Move> generateMovesForPiece(const Board& board, int row, int col)
     return {};
 }
 
+std::vector<Move> generateMovesForColor(const Board& board, PieceColor color) {
+    std::vector<Move> allMoves;
+
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            if (board.getPieceColor(row, col) == color) {
+                std::vector<Move> pieceMoves = generateMovesForPiece(board, row, col);
+                allMoves.insert(allMoves.end(), pieceMoves.begin(), pieceMoves.end());
+            }
+        }
+    }
+    return allMoves;
+}
